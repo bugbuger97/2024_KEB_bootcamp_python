b62ff8f87a311e20b7a7964e354dbe9533540391
bugbuger97 <junopaeng@gmail.com>
2024-01-15 14:29:52 +0900

    ãv1.0

--- day2.py
+++ day2.py
@@ -1,6 +1,6 @@
-dec = 65 # 10진수
-octal = 0o101 # 8진수
-hexadecimal = 0x41 # 16진수
-binary = 0b01000001 # 2진수
-print(bin(dec),bin(octal),bin(hexadecimal))
-print(ord('B'),ord('Z'),ord('a'),ord('0')) # 66, 90, 97, 48(아라비안 숫자는 48부터 시작함.)
+# 화씨 -> 섭씨
+def Fahrenheit_Celsius(F):
+    return (F-32) * (5/9)
+F = float(input('Input Fahrenheit : '))
+print('화씨 : %dF, 섭씨 : %0.3fC' %(F,Fahrenheit_Celsius(F)))
+print(f'화씨 : {F}F, 섭씨 : {Fahrenheit_Celsius(F):.3f}C') # f-string 소수점 자릿수 설정 -> :.3f이다.
