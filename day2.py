b547dfc52e4090be21e920bae9961ec6a7da7195
bugbuger97 <junopaeng@gmail.com>
2024-01-15 13:22:01 +0900

    v0.6

--- day2.py
+++ day2.py
@@ -1,22 +1,12 @@
-x = 2
-y = x + 5  # NameError: name 'x' is not defined
-print(y)
+base_number = int(input('Input base number : '))
+exponent_number = int(input('Input exponent number : '))
 
-print(type(3.14))
-print(type(3.14) == float)
+# f-string
+print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {pow(base_number, exponent_number)}')
 
-'''
-isinstance() is one of Python's built-in functions, 
-which is used to check whether a given object is an instance of a specified class or one of the classes. 
-This function can check whether it is a parent class, a child class, or an instance of the same class.
-isinstance(object, classinfo)
-'''
-print(isinstance(3.14, float))
-print(isinstance("Inha", float))
-print(isinstance(55, float))
+# format function -> {}안에 0,1,2를 넣어서 출력할 변수의 순서를 정할 수 있다.
+print('밑은 {}, 지수는{}, 결과 값은 {}'.format(base_number,exponent_number,pow(base_number, exponent_number)))
+print('밑은 {0}, 지수는{1}, 결과 값은 {2}'.format(base_number,exponent_number,pow(base_number, exponent_number)))
 
-artists = ['BTS', '뉴진스', '핑클', 'SES', 'HOT', '블랙핑크']
-groups = artists
-print(artists, groups)
-artists[2] = '세븐틴'
-print(artists, groups)
+# like C
+print('밑은 %d, 지수는 %d, 결과 값은 %d' %(base_number, exponent_number, pow(base_number, exponent_number)))
