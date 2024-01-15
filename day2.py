f437e10a2c2378b3456a883945d0317681496e5e
bugbuger97 <junopaeng@gmail.com>
2024-01-15 11:51:48 +0900

    v0.3

--- day2.py
+++ day2.py
@@ -1,7 +1,11 @@
-money = 5,000,000 # , : tuple
-print(money)
-print(type(money)) # tuple
+# money = 5,000,000 # , : tuple
+# print(money)
+# print(type(money)) # tuple
+#
+# cash = 5_000_000
+# print(cash)
+# print(type(cash))
 
-cash = 5_000_000
-print(cash)
-print(type(cash))
+base_number = int(input('Input base number : '))
+exponent_number = int(input('Input exponent number : '))
+print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number**exponent_number}')
