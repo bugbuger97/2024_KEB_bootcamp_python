7f9cb29e4970fe205a469b990f0531a59021fbd0
bugbuger97 <junopaeng@gmail.com>
2024-01-15 14:14:36 +0900

    v0.9

--- day2.py
+++ day2.py
@@ -1,11 +1,6 @@
-# divmod -> 파이썬의 내장 함수로서, divmod(나눌 대상, 나눌 값)으로 return (quotient, remainder)이다.
-print(divmod(10,2))
-print(f'몫은 {divmod(10,2)[0]}\n나머지는 {divmod(10,2)[1]}')
-
 dec = 65 # 10진수
 octal = 0o101 # 8진수
 hexadecimal = 0x41 # 16진수
 binary = 0b01000001 # 2진수
-print(dec,octal,hexadecimal,binary)
-print(chr(binary)) # chr(number) : 숫자 -> 문자
-print(ord('A')) # ord(number) : 문자 -> 숫자
+print(bin(dec),bin(octal),bin(hexadecimal))
+print(ord('B'),ord('Z'),ord('a'),ord('0')) # 66, 90, 97, 48(아라비안 숫자는 48부터 시작함.)
