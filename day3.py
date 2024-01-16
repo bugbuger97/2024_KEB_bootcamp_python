582a63771f800b289b6dc6a3cac424c6a871adf4
bugbuger97 <junopaeng@gmail.com>
2024-01-16 14:02:59 +0900

    v1.0

--- day3.py
+++ day3.py
@@ -1,20 +1,13 @@
-contents = 'How many times does the three-letter sequence the occur?'
-word = 'the'
-print(contents.count(word))
-print(word.isalnum()) # isalnum() : 문자열이 영어, 한글 혹은 숫자로 되어있으면 참 리턴, 아니면 거짓 리턴.
-print(contents.isalnum()) # 공백이 있기 때문에 Flase가 됨.
+subjects = {'python':'kim','c++':'sung','datastructure':'kim','database':'kang'}
+print("{0[c++]} {0[database]}".format(subjects))
 
-setup = 'a duck goes into a bar....'
-print(setup.rstrip('.'))
-print(setup.capitalize())
-print(setup.center(30))
-
-num = '7.03'
-print('%s' % 7.03)
-print('%f' % 7.03)
-print('%e' % 7.0345) # 지수형 부동소수
-print('%g' % 7.03) # 일반형: 값에 따라 %e 혹은 %f 사용
-print('%d%%' % 100) # %를 출력시키려면 %를 2번 입력함.
-
-strings = '   111woodchck'
-print('%-3.8s' % strings)
+# prime number
+number = int(input('Input Number : '))
+i = 2
+while i < number:
+    if number % i == 0:
+        print(f'{number} is not prime number')
+        break
+    i += 1
+if i == number:
+    print(f'{number} is prime number')
