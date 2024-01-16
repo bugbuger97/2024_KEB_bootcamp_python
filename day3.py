9bba11a8213a05697e085c7dd0442433664e4115
bugbuger97 <junopaeng@gmail.com>
2024-01-16 14:37:22 +0900

    v1.1

--- day3.py
+++ day3.py
@@ -1,13 +1,16 @@
-subjects = {'python':'kim','c++':'sung','datastructure':'kim','database':'kang'}
-print("{0[c++]} {0[database]}".format(subjects))
-
 # prime number
-number = int(input('Input Number : '))
-i = 2
-while i < number:
-    if number % i == 0:
+def judge_prime(number):
+    i = 2
+    while i < number:
+        if number % i == 0:
+            return False
+        i += 1
+    if i == number:
+        return True
+
+if __name__ == '__main__':
+    number = int(input('Input Number : '))
+    if judge_prime(number):
+        print(f'{number} is prime number')
+    else:
         print(f'{number} is not prime number')
-        break
-    i += 1
-if i == number:
-    print(f'{number} is prime number')
