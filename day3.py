055b601877b6039b53aa0304d2a8e7d3c2fc6d7d
bugbuger97 <junopaeng@gmail.com>
2024-01-16 15:54:13 +0900

    assignment_day3

--- day3.py
+++ day3.py
@@ -1,17 +1,45 @@
-# prime number
-numbers = input("Input first second number : ").split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
-if n1 > n2:
-    n1, n2 = n2, n1
+def Fahrenheit_Celsius(num):
+    return (num - 32) * (5/9)
+def Celsius_Fahrenheit(num):
+    return (num * (9/5)) + 32
 
-for number in range(n1, n2+1):
-    is_prime = True
-    if number < 2:
-        pass # continue
-    else:
-        for i in range(2, number):
+def judge_prime(number):
+    if number != 1:
+        for i in range(2,number):
             if number % i == 0:
-                is_prime = False
+                return False
+        return True
+def prime(num1, num2):
+    if num1 > num2:
+        num1, num2 = num2, num1
+    prime_number = list(range(num1 + 1, num2))
+    for i in range(num1+1, num2):
+        for j in range(2,i):
+            if i % j == 0:
+                prime_number.remove(i)
                 break
-        if is_prime: print(number, end=' ')
+    return prime_number
+
+if __name__ == '__main__':
+    while True:
+        menu = input('menu : 1) Fahrenheit -> Celsius 2) Celsius -> Fahrenheit 3) 소수 판정 4) 두 수 사이의 소수 나열 5) Terminated program : ')
+        if menu == '1':
+            num = float(input('Input Fahrenheit : '))
+            print(f'{num}F -> {Fahrenheit_Celsius(num):.2f}C')
+        elif menu == '2':
+            num = float(input('Input Celsius : '))
+            print(f'{num}C -> {Celsius_Fahrenheit(num):.2f}F')
+        elif menu == '3':
+            number = int(input('Input Number : '))
+            if judge_prime(number):
+                print(f'{number} is prime number')
+            else:
+                print(f'{number} is not prime number')
+        elif menu == '4':
+            num1, num2 = input('Input Num1, Num2 : ').split()
+            print(*prime(int(num1), int(num2)))
+        elif menu == '5':
+            print('Terminated program')
+            break
+        else:
+            print('Error')
