e33498b86366380f446f63add4e4c37d78dfb61f
bugbuger97 <junopaeng@gmail.com>
2024-01-16 13:36:53 +0900

    v0.9

--- day3.py
+++ day3.py
@@ -1,6 +1,20 @@
-subjects = "python c++ database linux"
-subject = input('Input Subject : ')
-try:
-    print(f'해당 과목 {subject}은 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.')
-except ValueError:
-    print(f'해당 과목 {subject}이 존재하지 않습니다.')
+contents = 'How many times does the three-letter sequence the occur?'
+word = 'the'
+print(contents.count(word))
+print(word.isalnum()) # isalnum() : 문자열이 영어, 한글 혹은 숫자로 되어있으면 참 리턴, 아니면 거짓 리턴.
+print(contents.isalnum()) # 공백이 있기 때문에 Flase가 됨.
+
+setup = 'a duck goes into a bar....'
+print(setup.rstrip('.'))
+print(setup.capitalize())
+print(setup.center(30))
+
+num = '7.03'
+print('%s' % 7.03)
+print('%f' % 7.03)
+print('%e' % 7.0345) # 지수형 부동소수
+print('%g' % 7.03) # 일반형: 값에 따라 %e 혹은 %f 사용
+print('%d%%' % 100) # %를 출력시키려면 %를 2번 입력함.
+
+strings = '   111woodchck'
+print('%-3.8s' % strings)
