523de463943972630ad74af2a36552b02f508291
bugbuger97 <junopaeng@gmail.com>
2024-01-16 15:27:14 +0900

    v1.4

--- day3.py
+++ day3.py
@@ -1,14 +1,20 @@
-def judge_prime(number):
-    if number != 1:
-        for i in range(2,number):
-            if number % i == 0:
-                return False
-        return True
-
+def judge_prime(num1, num2):
+    if num1 <= num2:
+        prime_number = list(range(num1 + 1, num2))
+        for i in range(num1+1, num2):
+            for j in range(2,i):
+                if i % j == 0:
+                    prime_number.remove(i)
+                    break
+    else:
+        prime_number = list(range(num2 + 1, num1))
+        for i in range(num2 + 1, num1):
+            for j in range(2, i):
+                if i % j == 0:
+                    prime_number.remove(i)
+                    break
+    return prime_number
 
 if __name__ == '__main__':
-    number = int(input('Input Number : '))
-    if judge_prime(number):
-        print(f'{number} is prime number')
-    else:
-        print(f'{number} is not prime number')
+    num1,num2 = input('Input Num1, Num2 : ').split()
+    print(*judge_prime(int(num1), int(num2)))
