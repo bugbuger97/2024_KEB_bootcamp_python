aeaf15f798b900f616f1b970cfd6c8f07915721e
bugbuger97 <junopaeng@gmail.com>
2024-01-16 11:45:03 +0900

    v0.8

--- day3.py
+++ day3.py
@@ -1,5 +1,6 @@
 subjects = "python c++ database linux"
-if subjects.find('c++') != -1:
-    print(f'해당 과목 c++은 존재합니다. 위치는 {subjects.find('c++')}번 인덱스입니다.')
-else:
-    print(f'해당 과목 c++이 존재하지 않습니다.')
+subject = input('Input Subject : ')
+try:
+    print(f'해당 과목 {subject}은 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.')
+except ValueError:
+    print(f'해당 과목 {subject}이 존재하지 않습니다.')
