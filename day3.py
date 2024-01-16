891dd5880b54c9848252faa696392ff752ebcda1
bugbuger97 <junopaeng@gmail.com>
2024-01-16 11:35:34 +0900

    v0.6

--- day3.py
+++ day3.py
@@ -1,16 +1,22 @@
-# string split
-course = "2024 KEB bootcamp"
+# replace()
+course = "KEB 2024 KEB Bootcamp"
 print(course)
-print(course.split()) # split의 구분자 ' '(스페이스 바)이다.
-print(course.split('B')) # split의 구분자 'B'이다.
+# course = course.replace('KEB','Inha') # replace update
+#course = course.replace('KEB','Inha',1)
+#print(course)
 
-# numbers = input('FirstNumber SecondNumber : ').split() # type -> list
-# print(numbers)
-# print(numbers[0] + numbers[1]) # concatenation
-# print(int(numbers[0]) + int(numbers[1])) # arithmetic operation
+# strip() : 불필요한 좌우 공백 제거
+# lstrip() : 불필요한 왼쪽 공백 제거
+# rstrip() : 불필요한 오른쪽 공백 제거
+blurt = "What? the...!!?????"
+print(blurt)
+blurt = blurt.strip('.!?')
+print(blurt) # 중간에 들어가 있는 특수기호는 제거하지 못함.
 
-# join() : list -> string
-# '원소 사이에 집어 넣을 문자'.join(문자열로 바꿀 리스트)
-subjects = ['python','c++','database']
-subjects_string = ' / '.join(subjects)
-print(subjects_string)
+# startswith('내용') : '내용'으로 시작하나? 맞으면 -> True
+# endswith('내용') : '내용'으로 끝나나? 맞으면 -> True
+# find('내용') : '내용'의 인덱스 번호 반환
+# rfind('내용') : 리버스 파인트 -> 오른쪽부터 찾으면서 '내용'의 인덱스 번호 반환
+# find에서 -1이 반환 되면 '내용'이 없는 것이다.
+print(course.find('KEB'))
+print(course.index('2024'))
