3d58223dd9bf2e23cb4055e28980023d39249de7
bugbuger97 <junopaeng@gmail.com>
2024-01-17 15:23:15 +0900

    v0.9

--- day4.py
+++ day4.py
@@ -1,11 +1,11 @@
+import random
 drinks_foods = {"위스키": "초콜릿", "와인": "치즈", "소주": "삽겹살", "고량주": "양꼬치"}
-
 #drink = input(drinks_foods.keys())
 drinks_foods_keys = list(drinks_foods)
 #print(drinks_foods_keys)
 
 while True:
-    menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods_keys[0]}   2) {drinks_foods_keys[1]}   3) {drinks_foods_keys[2]}   4) {drinks_foods_keys[3]}   5) 종료 : ')
+    menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods_keys[0]}   2) {drinks_foods_keys[1]}   3) {drinks_foods_keys[2]}   4) {drinks_foods_keys[3]}   5) 아무거나  6) 종료 : ')
     if menu == '1':
         print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다')
     elif menu == '2':
@@ -15,5 +15,8 @@ while True:
     elif menu == '4':
         print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다')
     elif menu == '5':
+        n = random.randint(0,3)
+        print(f'{drinks_foods_keys[n]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[n]]} 입니다')
+    elif menu == '6':
         print(f'다음에 또 오세요')
         break
