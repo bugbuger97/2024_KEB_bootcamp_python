e3309a2ed29c6631145d57b1d08c71c6a22f6bb4
bugbuger97 <junopaeng@gmail.com>
2024-01-17 13:28:39 +0900

    v0.4

--- day4.py
+++ day4.py
@@ -1,13 +1,20 @@
-# 튜플은 바꿀 수 없다. 그러나 리스트로 바꾼 다음 값을 수정하고 다시 튜플로 바꿔 준다.
-# split() : 문자열 -> 리스트로 변경시켜줌.
+# shallow copy
+subjects = ["a","b","c"]
+a = subjects # shallow copy
+b = subjects.copy() # shallow copy
+c = list(subjects) # shallow copy
+d = subjects[:] # shallow copy
+print(subjects,a,b,c,d)
+subjects[1] = "x"
+print(subjects,a,b,c,d)
 
-subjects = ["C++","Java","Python"]
-print(subjects[::-1]) # 리스트 반대로 출력함. 그러나 원본은 그대로임.
-print(subjects)
-subjects.reverse() # 원본이 원래 리스트 반대로 순서가 바뀐다.
-print(subjects)
-
-numbers = [1,2,3,4]
-print(numbers)
-numbers[1:3] = []
-print(numbers)
+import copy
+subjects = ["a",["b","c"],"d"]
+a = subjects # shallow copy
+b = subjects.copy() # shallow copy
+c = list(subjects) # shallow copy
+d = subjects[:] # shallow copy
+e = copy.deepcopy(subjects) # deep copy
+print(subjects,a,b,c,d,e)
+subjects[1][1]= "x"
+print(subjects,a,b,c,d,e) # deep copy인 e만 변하지 않는 것을 볼 수 있다.
