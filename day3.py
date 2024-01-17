10a4a6605b96c6ae69f5223fb3c171ee357d0e5b
bugbuger97 <junopaeng@gmail.com>
2024-01-17 09:14:52 +0900

    assignment_day3-1

--- day3.py
+++ day3.py
@@ -1,45 +1,26 @@
-def Fahrenheit_Celsius(num):
-    return (num - 32) * (5/9)
-def Celsius_Fahrenheit(num):
-    return (num * (9/5)) + 32
+# 6.1
+result = [i for i in range(3,-1,-1)]
+print(result)
 
-def judge_prime(number):
-    if number != 1:
-        for i in range(2,number):
-            if number % i == 0:
-                return False
-        return True
-def prime(num1, num2):
-    if num1 > num2:
-        num1, num2 = num2, num1
-    prime_number = list(range(num1 + 1, num2))
-    for i in range(num1+1, num2):
-        for j in range(2,i):
-            if i % j == 0:
-                prime_number.remove(i)
-                break
-    return prime_number
+# 6.2
+guess_me = 7
+number = 1
+while True:
+    if number < guess_me:
+        print('too low')
+    elif number == guess_me:
+        print('found it!')
+    else:
+        print('oops')
+        break
+    number += 1
 
-if __name__ == '__main__':
-    while True:
-        menu = input('menu : 1) Fahrenheit -> Celsius 2) Celsius -> Fahrenheit 3) 소수 판정 4) 두 수 사이의 소수 나열 5) Terminated program : ')
-        if menu == '1':
-            num = float(input('Input Fahrenheit : '))
-            print(f'{num}F -> {Fahrenheit_Celsius(num):.2f}C')
-        elif menu == '2':
-            num = float(input('Input Celsius : '))
-            print(f'{num}C -> {Celsius_Fahrenheit(num):.2f}F')
-        elif menu == '3':
-            number = int(input('Input Number : '))
-            if judge_prime(number):
-                print(f'{number} is prime number')
-            else:
-                print(f'{number} is not prime number')
-        elif menu == '4':
-            num1, num2 = input('Input Num1, Num2 : ').split()
-            print(*prime(int(num1), int(num2)))
-        elif menu == '5':
-            print('Terminated program')
-            break
-        else:
-            print('Error')
+# 6.3
+guess_me = 5
+for number in range(10):
+    if number < guess_me:
+        print('too low')
+    elif number == guess_me:
+        print('found it!')
+    else:
+        print('oops')
