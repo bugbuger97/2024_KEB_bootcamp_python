8c90c1404086b9c3468514684510d2c6eeaf798d
bugbuger97 <junopaeng@gmail.com>
2024-01-17 12:57:26 +0900

    v0.3

--- day4.py
+++ day4.py
@@ -1,29 +1,13 @@
-# Tuple
-t1 = (5)
-t2 = 5,
-t3 = (5,)
-t4 = (5,7)
-t5 = 5,7
-print(type(t1),type(t2),type(t3),type(t4),type(t5))
-t6 = "python", "kim" # 이것을 packing이라고 함.
-print(type(t6), t6[1])
+# 튜플은 바꿀 수 없다. 그러나 리스트로 바꾼 다음 값을 수정하고 다시 튜플로 바꿔 준다.
+# split() : 문자열 -> 리스트로 변경시켜줌.
 
-subject, prof = t6 # unpacking -> unpacking할 때, 정확히 개수가 맞아야 한다.
-print(prof) # kim
-print(subject) # python
+subjects = ["C++","Java","Python"]
+print(subjects[::-1]) # 리스트 반대로 출력함. 그러나 원본은 그대로임.
+print(subjects)
+subjects.reverse() # 원본이 원래 리스트 반대로 순서가 바뀐다.
+print(subjects)
 
-t7 = ()
-t8 = tuple()
-print(type(t7),type(t8))
-
-t9 = ('abc')
-print(type(t9)) # str
-
-print(type(9,), type(((9,)))) # int, tuple
-
-# tuple간의 비교 연산이 가능함.
-# '+' 연산으로 튜플간의 합치기 가능함.
-t10 = 4.43,
-t11 = 3.97, 4.1, 3.27
-print(t10+t11) # (4.43, 3.97, 4.1, 3.27)
-print(t11+t10) # (3.97, 4.1, 3.27, 4.43)
+numbers = [1,2,3,4]
+print(numbers)
+numbers[1:3] = []
+print(numbers)
