14a3e842ea31ab143b01ea5b6410d084086eed72
bugbuger97 <junopaeng@gmail.com>
2024-01-18 17:56:25 +0900

    day5_Variable_length_Arguments_and_Variable_length_keyword_Arguments

--- day5.py
+++ day5.py
@@ -1,25 +1,24 @@
-# positional argument : 인수 위치의 순서에 따라 인수가 들어갈 매개변수가 정해짐.
-# keyword argument : 위치 인수의 혼란을 피하기 위해 매개변수에 상응하는 이름을 인수에 지정할 수 있다.
-def AI(ML, DL): # positional argument
-    return {'ML':ML,'DL':DL}
-print(AI('Linear Regression', 'chat GPT'))
+# Variable length Arguments (가변 인수) : 파이썬에서는 함수에서 입력값이 몇 개가 될지 모를 때을 위해 가변 인수를 튜플 타입으로 제공함.
+# *args : 앞에 애스터리스크(*)를 붙여주는 것이 특징이다.
+def print_Variable_length_Arguments(*args):
+    print('Positional tuple :',args)
+print_Variable_length_Arguments(1,2,3,4,5)
+# *args는 함수 외부에서 여러 개로 이루어진 값들을 함수 내부에 튜플 형태로 모은다.
 
-def AI(ML=None, DL='INDUCK'): # keyword argument
-    return {'ML':ML,'DL':DL}
-print(AI())
+# Variable length keyword Arguments (가변 키워드 인수) : 키워드 인수를 딕셔너리로 묶기 위해 2개의 애스터리스크(**)를 사용하여, 인수의 이름은 key, 이 key에 대응하는 딕셔너리 값이 value임.
+def print_Variable_length_keyword_Arguments(**kwargs):
+    print('Keyword arguments :',kwargs)
+print_Variable_length_keyword_Arguments()
+print_Variable_length_keyword_Arguments(university='inha')
+# 함수 외부에서 **kwargs는 딕셔너리 kwargs를 '이름=값' 인수로 분해함.
+# 함수 내부에서 **kwargs는 '이름=값' 인수를 단일 딕셔너리 매개변수 kwargs에 모은다.
 
-# 기본 인수는 함수가 실행될 때, 계산 되지 않고, 함수를 정의할 때, 계산됨.
-
-def buggy(arg,result=[]): # argument 함수 괄호 안의 변수(외부)
-    result.append(arg) # parameter 함수 내부 변수
-    print(result)
-buggy('A') # ['A']
-buggy('B') # ['A', 'B']
-# A, B가 리스트에 저장되어 있는 상태로 출력됨.
-def nonbuggy(arg,result=None):
-    if result is None:
-        result=list()
-    result.append(arg)
-    print(result)
-nonbuggy('A') # ['A']
-nonbuggy('B') # ['B']
+# 키워드 전용 인수 : 값을 위치적으로 제공하지 않고, '이름=값'으로 제공해야함.
+def my_print_data(data,*,start=0,end=100): # 함수 정의에서 단일 애스터리스크(*)는 start 및 end 매개변수의 기본값을 사용하고 싶지 않은 경우, 명명된 인수로 제공해야 함.
+    for value in (data[start:end]):
+        print(value)
+data = [0,1,2,3,4,5,6,7,8]
+# my_print_data(data)
+# my_print_data(data,start=5)
+my_print_data(data,end=4)
+# 함수에 인수를 전달할 때, 인수가 가변 객체인 경우, 해당 매개변수를 통해 함수 내부에서 값을 변경할 수 있음.
