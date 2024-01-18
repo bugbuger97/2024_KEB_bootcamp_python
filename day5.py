4715c4495821c20ce0d9a7db9c66425dd2b1e82e
bugbuger97 <junopaeng@gmail.com>
2024-01-18 12:26:13 +0900

    day5_v0.8

--- day5.py
+++ day5.py
@@ -1,28 +1,22 @@
-# is라는 문법을 이용해서 False와 None을 구분할 수 있다.
-thing = None  # False
-if thing is None:
-    print('thing is Nothing')
-else:
-    print('thing is something')
+# 파이썬에서는 오버로딩을 지원하지 않는다.
+def a(n1,n2):
+    print(n1,n2)
+def a(n):
+    print(n)
 
-# Positional Arguments -> def f(x,y) : 순서에 관계가 있다.
-# Keyword Arguments -> def f(x=None,y=1)
+a(1) # 1
+#a(1,2) # TypeError: a() takes 1 positional argument but 2 were given
 
-def buggy(arg, result = []):
-    result.append(arg)
-    print(result)
-buggy(1) # [1]
-buggy(2) # [1, 2] -> 계속 1이 들어가 있다.
+def b(n):
+    if n is None:
+        print(f'{n} is None')
+    elif n:
+        print(f'{n} is True')
+    else:
+        print(f'{n} is False')
 
-def nonbuggy(arg, result = None):
-    if result is None:
-        result = []
-    result.append(arg)
-    print(result)
-nonbuggy(1) # [1]
-nonbuggy(2) # [2] -> 겹쳐지지 않는 것을 볼 수 있다.
-
-# 가변 인수
-def args(*arg):
-    print('positional tuple :',arg)
-args(1) # 함수의 매개변수에 애스터 리스크(*)를 사용할 때, 애스터 리스크(*)는 매개변수에서 위치 인수 변수를 튜플로 묶음.
+b([]) # False
+b([0]) # True
+b(0) # False
+b(None) # None
+b('') # False
