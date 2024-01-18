13a545cc7bd473768764622c86b66890b24f88ea
bugbuger97 <junopaeng@gmail.com>
2024-01-18 09:57:53 +0900

    day5_v0.4

--- day5.py
+++ day5.py
@@ -1,16 +1,17 @@
-# dict comprehensions
-# 딕셔너리에서 immutable한 것들만 키값이 될수 있다.
+# set : key container -> unorder
 
-# univ = 'inha university'
-# counts_alphabet = {letter: univ.count(letter) for letter in univ}
-# print(counts_alphabet)
+alphabet = set('abcdefghhhhh')
+print(alphabet) # 중복 없이 무작위 순서로 출력 되어질 수 있음.
 
-# univ = 'inha university'
-# counts_alphabet=dict()
-# for letter in univ:
-#     counts_alphabet[letter] = univ.count(letter)
-# print(counts_alphabet)
+# set1.intersection(set2) : 교집합(&)
+# set1.union(set2) : 합집합(|)
+# set1.difference(set2) : 차집합(-)
+# set1.symmetric_difference(set2) : 대칭 차집합(^)
 
-# 8.10
-squares = {n : n*n for n in range(10)}
-print(squares)
+s = set([1,2,3,3,5])
+s.add(6)
+print(s)
+
+# Create immutable set
+fs = frozenset([1,2,3,3,3,5,5])
+# fs.add(4) # AttributeError: 'frozenset' object has no attribute 'add'
