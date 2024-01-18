df658ec2273edb900b1ad26b01d4abce6d9c8692
bugbuger97 <junopaeng@gmail.com>
2024-01-18 11:44:45 +0900

    day5_v0.7

--- day5.py
+++ day5.py
@@ -1,69 +1,28 @@
-# prime number with fuction
-def is_prime(number) -> bool: # -> bool은 함수의 결과값에 hint를 줌으로서 가독성을 높인다.
-    '''
-    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean 타입으로 리턴
-    :param number: 판정할 매개변수
-    :return: 소수면 True 소수가 아니면 Flase
-    '''
-    if number > 1:
-        for i in range(2,number):
-            if number % i == 0:
-                return False
-        return True
-    return False
-def Fahrenheit_Celsius(num) -> float:
-    '''
-    Fahrenheit temperature -> Celsius temperature
-    :param num: Fahrenheit temperature(float)
-    :return: Celsius temperature(float)
-    '''
-    return (num - 32) * (5/9)
-def Celsius_Fahrenheit(num) -> float:
-    '''
-    Celsius temperature -> Fahrenheit temperature
-    :param num: Celsius temperature(float)
-    :return: Fahrenheit temperature(float)
-    '''
-    return (num * (9/5)) + 32
-def prime(num1, num2) -> list:
-    '''
-    (num1, num2) 두 수 사이의 소수를 나열하여 출력
-    :param num1: num1, num2
-    :param num2: 두 수 사이의 소수를 리스트로 반환
-    :return:
-    '''
-    if num1 > num2:
-        num1, num2 = num2, num1
-    prime_number = list(range(num1 + 1, num2))
-    for i in range(num1+1, num2):
-        for j in range(2,i):
-            if i % j == 0:
-                prime_number.remove(i)
-                break
-    return prime_number
+# is라는 문법을 이용해서 False와 None을 구분할 수 있다.
+thing = None  # False
+if thing is None:
+    print('thing is Nothing')
+else:
+    print('thing is something')
 
+# Positional Arguments -> def f(x,y) : 순서에 관계가 있다.
+# Keyword Arguments -> def f(x=None,y=1)
 
-if __name__ == "__main__":
-    while True:
-        menu = input(
-            'menu : 1) Fahrenheit -> Celsius 2) Celsius -> Fahrenheit 3) 소수 판정 4) 두 수 사이의 소수 나열 5) Terminated program : ')
-        if menu == '1':
-            num = float(input('Input Fahrenheit : '))
-            print(f'{num}F -> {Fahrenheit_Celsius(num):.2f}C')
-        elif menu == '2':
-            num = float(input('Input Celsius : '))
-            print(f'{num}C -> {Celsius_Fahrenheit(num):.2f}F')
-        elif menu == '3':
-            number = int(input('Input Number : '))
-            if is_prime(number):
-                print(f'{number} is prime number')
-            else:
-                print(f'{number} is not prime number')
-        elif menu == '4':
-            num1, num2 = input('Input Num1, Num2 : ').split()
-            print(*prime(int(num1), int(num2)))
-        elif menu == '5':
-            print('Terminated program')
-            break
-        else:
-            print('Error')
+def buggy(arg, result = []):
+    result.append(arg)
+    print(result)
+buggy(1) # [1]
+buggy(2) # [1, 2] -> 계속 1이 들어가 있다.
+
+def nonbuggy(arg, result = None):
+    if result is None:
+        result = []
+    result.append(arg)
+    print(result)
+nonbuggy(1) # [1]
+nonbuggy(2) # [2] -> 겹쳐지지 않는 것을 볼 수 있다.
+
+# 가변 인수
+def args(*arg):
+    print('positional tuple :',arg)
+args(1) # 함수의 매개변수에 애스터 리스크(*)를 사용할 때, 애스터 리스크(*)는 매개변수에서 위치 인수 변수를 튜플로 묶음.
