28aa0645d847e7efd2cef989dda4760cf2c940de
bugbuger97 <junopaeng@gmail.com>
2024-01-18 16:11:27 +0900

    day5_ãparameter_argument

--- day5.py
+++ day5.py
@@ -1,53 +1,9 @@
-# decorators
-# 개방 폐쇄 원칙 : 확장에는 열려있고, 수정에는 닫혀있어야 한다. -> 클로저 사용함.
-# decorators는 원래 함수에 뭔가 더 덧붙여주어서 함수를 확장해주는 것을 말한다.
+# 매개변수(parameter) : 함수의 정의에서 전달받은 인수를 함수 내부로 전달하기 위해 사용하는 변수를 의미함.
+# 인수(argument) : 함수가 호출될 때 함수로 값을 전달해주는 값임.
 
-# decorator
-def description(f):  # closure
-    def inner(*args):
-        print(f.__name__)
-        print(f.__doc__)
-        r = f(*args)
-        return r
-    return inner
+def my_print(sometihing): # something -> 매개변수(parameter)
+    print(sometihing + ' ' + sometihing)
 
+my_print('Inha University') # 'Inha University' -> 인수(argument)
 
-def squares(n):
-    """
-    제곱 함수
-    """
-    return n * n
-
-@description
-def power(b, e):
-    """
-    거듭제곱 함수
-    """
-    result = 1
-    for _ in range(e):
-        result = result * b
-    return result
-
-
-f1 = description(squares)
-print(f1(9))
-print(power(2, 10))
-# f2 = description(power)
-# print(f2(2, 10))
-
-# print(squares(7))
-# print(squares.__doc__)
-
-# def my_range(first=0, last=5, step=1):
-#     number = first
-#     while number < last:
-#         yield number
-#         number += step
-#
-# r = my_range()
-# print(r, type(r))
-#
-# for x in r:
-#     print(x)
-# for x in r:
-#     print(x)
+# 함수 외부에서는 인수라고 하지만 내부에서는 매개변수하고 함.
