88d3bfe33c7ed461a8536629003e92ff3dfeb57b
bugbuger97 <junopaeng@gmail.com>
2024-01-18 15:44:51 +0900

    day5_v1.5

--- day5.py
+++ day5.py
@@ -1,14 +1,53 @@
-# generators : 반복자(iterators)을 위한 데이터의 자원이다. -> it keeps track of where it was the last time it was called and returns the next value.
-# generator function : yield
+# decorators
+# 개방 폐쇄 원칙 : 확장에는 열려있고, 수정에는 닫혀있어야 한다. -> 클로저 사용함.
+# decorators는 원래 함수에 뭔가 더 덧붙여주어서 함수를 확장해주는 것을 말한다.
 
-def my_range(first=0, last=5, step =1):
-    number = first
-    while number < last:
-        yield number
-        number += step
+# decorator
+def description(f):  # closure
+    def inner(*args):
+        print(f.__name__)
+        print(f.__doc__)
+        r = f(*args)
+        return r
+    return inner
 
-r = my_range()
-print(r,type(r))
 
-for x in r: print(x)
-for x in r: print(x) # 2번째에서는 아무것도 나오지 않는다. -> 한번 꺼내서 쓰면 남아 있지 않아서 똑같은 결과가 나오지 않는다.
+def squares(n):
+    """
+    제곱 함수
+    """
+    return n * n
+
+@description
+def power(b, e):
+    """
+    거듭제곱 함수
+    """
+    result = 1
+    for _ in range(e):
+        result = result * b
+    return result
+
+
+f1 = description(squares)
+print(f1(9))
+print(power(2, 10))
+# f2 = description(power)
+# print(f2(2, 10))
+
+# print(squares(7))
+# print(squares.__doc__)
+
+# def my_range(first=0, last=5, step=1):
+#     number = first
+#     while number < last:
+#         yield number
+#         number += step
+#
+# r = my_range()
+# print(r, type(r))
+#
+# for x in r:
+#     print(x)
+# for x in r:
+#     print(x)
