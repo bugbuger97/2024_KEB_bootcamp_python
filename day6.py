2e6e5018178891271a73deca862fa200762d13dd
bugbuger97 <junopaeng@gmail.com>
2024-01-19 13:09:23 +0900

    day6_v0.8

--- day6.py
+++ day6.py
@@ -1,21 +1,8 @@
-# def desc():
-#     def w():
-#         print("w")
-#     print("a")
-#     return w
-#
-# desc() # a -> w()는 출력 되지 않음.
+class Pokemon: # 클래스의  앞 글자는 대문자로 시작함.
+    def __init__(self, name):
+        print(f'{name} 포켓몬스터 생성')
 
-def desc(f):
-    def study():
-        print("study")
-        f()
-    return study
-@desc
-def something():
-    print("do something~")
-
-# anything = desc(something)
-# anything()
-
-something()
+pikachu = Pokemon('피카츄') # 해당 객체당 init이 딱 한번 발생함.
+squirtle = Pokemon('꼬부기')
+print(pikachu)  # <__main__.Pokemon object at 0x000002C9BDBBD4C0> -> 메모리 번지가 출력됨.
+print(squirtle)
