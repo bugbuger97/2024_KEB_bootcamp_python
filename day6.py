9cde7b0176141063d32068cf4635810662717e40
bugbuger97 <junopaeng@gmail.com>
2024-01-19 10:54:48 +0900

    day6_v0.5

--- day6.py
+++ day6.py
@@ -1,22 +1,19 @@
-# 데코레이터를 여러 개를 붙일 수 있음.
+# Async Functions(비동기 함수) : 함수를 정의하는 def 앞에 async 키워드가 붙으면 비동기 함수임.
+# 함수를 호출하기 전에 await 키워드가 있으면 해당 함수는 비동기적임.
+# 비동기 함수는 실행을 완료하기보다 '제어를 넘겨주는 것'임.
 
-# Recursion
-def factorial_repetition(n) -> int:
-    result = 1
-    for i in range(2,n+1):
-        result = result * i
-    return result
+# 예외 처리
+import random
 
-def factorial_recursion(n) -> int:
-    '''
-    재귀함수를 사용한 팩토리얼 함수
-    :param n: 정수
-    :return: function, int
-    '''
-    if n == 1:
-        return n
-    else:
-        return n*factorial_recursion(n-1)
-
-print(factorial_repetition(5))
-print(factorial_recursion(6)) # 재귀는 반복문보다 속도가 느리다
+numbers = [random.randint(1,100) for i in range(5)]
+print(numbers)
+try:
+    pick = int(input(f'Input index(0 ~ {len(numbers)-1}) : '))
+    print(numbers[pick])
+    print(10/0)
+except IndexError as err:
+    print(f'인덱스 범위를 벗어났습니다.\n{err}') # err -> 시스템이 던져준 메시지임.
+except ValueError as err:
+    print(f'정수만을 입력해주세요.\n{err}')
+except Exception as err: # except Exception -> 이 부분은 보험으로 항상 맨 밑에 들어가야 한다.
+    print(f'Error occurs!\n{err}')
