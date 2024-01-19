58c904a6c1675b23d2c5247582b84de0cdcbe8af
bugbuger97 <junopaeng@gmail.com>
2024-01-19 11:23:49 +0900

    day6_v0.7

--- day6.py
+++ day6.py
@@ -1,20 +1,21 @@
-import random
-class OopsException(Exception):
-    pass
+# def desc():
+#     def w():
+#         print("w")
+#     print("a")
+#     return w
+#
+# desc() # a -> w()는 출력 되지 않음.
 
-numbers = [random.randint(1,100) for i in range(5)]
-print(numbers)
-try:
-    pick = int(input(f'Input index(0 ~ {len(numbers)-1}) : '))
-    print(numbers[pick])
-    raise OopsException("Oops") # exception!
-except IndexError as err:
-    print(f'인덱스 범위를 벗어났습니다.\n{err}')
-except ValueError as err:
-    print(f'정수만을 입력해주세요.\n{err}')
-except OopsException as err:
-    print(f'Oops Oops {err}')
-except Exception as err:
-    print(f'Error occurs!\n{err}')
-else:
-    print(f'Program terminate')
+def desc(f):
+    def study():
+        print("study")
+        f()
+    return study
+@desc
+def something():
+    print("do something~")
+
+# anything = desc(something)
+# anything()
+
+something()
