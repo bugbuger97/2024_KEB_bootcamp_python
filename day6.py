34e60d4213f6485402b6f597352a25d48c240ecf
bugbuger97 <junopaeng@gmail.com>
2024-01-19 15:27:25 +0900

    day6_v1.4

--- day6.py
+++ day6.py
@@ -1,37 +1,41 @@
-# 다중 상속 -> 파이썬에서는 클래스 괄호 안에 상속 받은(표기된) 순서대로 부모의 우선순위가 결정된다.
 class FlyingMixin:
     def fly(self):
-        return f'{self.name} fly~'
+        return f"{self.hidden_name}이(가) 하늘을 훨훨 날아갑니다~"
+
 class SwimmingMixin:
     def swim(self):
-        return f'{self.name} swim~'
+        return f"{self.hidden_name}이(가) 수영을 합니다."
+
 class Pokemon:
-    def __init__(self,name):
-        self.name = name
+    def __init__(self, name):
+        self.hidden_name = name
+
     def attack(self):
-        print('공격')
+        print("공격~")
+
     def get_name(self):
-        print("inside getter")
-        return self.name
-    def set_name(self,new_name):
-        print("inside setter")
-        self.name = new_name
-        return
-class Charizard(Pokemon,FlyingMixin):
+        return self.hidden_name
+
+    def set_name(self, new_name):
+        self.hidden_name = new_name
+
+    name = property(get_name, set_name)
+
+
+class Charizard(Pokemon, FlyingMixin):
     pass
-class Gyarados(Pokemon,SwimmingMixin):
+
+class Gyarados(Pokemon, SwimmingMixin):
     pass
 
 g1 = Gyarados("갸라도스")
 c1 = Charizard("리자몽")
-# print(g1.swim())
-# print(c1.fly())
-# c1.attack()
-# Charizard.attack(c1) # ()안에 객체가 와줘야 함.
-print(g1.name)
-g1.name = '잉어킹' # direct access(위험할 수도 있음.)
-print(g1.name)
 
-print(g1.get_name())
-g1.set_name('잉어')
-print(g1.get_name())
+# print(g1.get_name())
+# g1.set_name("잉어킹")
+# print(g1.get_name())
+
+# property
+print(g1.name)
+g1.name = "잉어킹"
+print(g1.name)
