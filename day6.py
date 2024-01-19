7696c46d6cd30baf7d37a8a57884c55989ccccd2
bugbuger97 <junopaeng@gmail.com>
2024-01-19 15:09:58 +0900

    day6_v1.2

--- day6.py
+++ day6.py
@@ -1,29 +1,19 @@
+# 다중 상속 -> 파이썬에서는 클래스 괄호 안에 상속 받은(표기된) 순서대로 부모의 우선순위가 결정된다.
+class FlyingMixin:
+    def fly(self):
+        return f'{self.name} fly~'
+class SwimmingMixin:
+    def swim(self):
+        return f'{self.name} swim~'
 class Pokemon:
     def __init__(self,name):
         self.name = name
-    def attack(self,target):
-        print(f'{self.name}이(가) {target.name}을(를) 공격!')
-class Pikachu(Pokemon): # inherit : is-a
-    def __init__(self,name,type):
-        super().__init__(name) # super() -> 부모의 init 메서드를 호출
-        self.type = type
-    def attack(self,target): # method override(대체)
-        print(f'{self.name}이(가) {target.name}을(를) 백만볼트 공격!')
-    def info(self):
-        print(f'{self.name}는(은) {self.type} 계열의 포켓몬입니다.')
-class Squirtle(Pokemon): # inherit : is-a
-    def attack(self,target):
-        print(f'{self.name}이(가) {target.name}을(를) 물대포 공격!')
-class Augumon:
+class Charizard(Pokemon,FlyingMixin):
+    pass
+class Gyarados(Pokemon,SwimmingMixin):
     pass
 
-p1 = Pikachu("피카츄","전기")
-p2 = Squirtle("꼬부기")
-p3 = Pokemon("아무무")
-print(p1.name) # 자식한테 .name이 없으면, 부모를 찾아간다.
-# 추상 클래스(abstract class) : 예시) 도형, 포유류, 운동 선수 그릴 수 없는 거대한 추상적인 개념의 클래스 (분류하기 위해 사용)
-print(issubclass(Pikachu,Pokemon))
-print(issubclass(Augumon,Pokemon))
-p1.attack(p2)
-p2.attack(p1)
-p1.info()
+g1 = Gyarados("갸라도스")
+c1 = Charizard("리자몽")
+print(g1.swim())
+print(c1.fly())
