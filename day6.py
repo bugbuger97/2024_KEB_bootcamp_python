8d86417f31755bcc87db9defb7779ecc5978bd18
bugbuger97 <junopaeng@gmail.com>
2024-01-19 13:20:52 +0900

    day6_v0.9

--- day6.py
+++ day6.py
@@ -1,8 +1,10 @@
-class Pokemon: # 클래스의  앞 글자는 대문자로 시작함.
-    def __init__(self, name):
-        print(f'{name} 포켓몬스터 생성')
+class Pokemon:
+    pass
 
-pikachu = Pokemon('피카츄') # 해당 객체당 init이 딱 한번 발생함.
-squirtle = Pokemon('꼬부기')
-print(pikachu)  # <__main__.Pokemon object at 0x000002C9BDBBD4C0> -> 메모리 번지가 출력됨.
-print(squirtle)
+pikachu = Pokemon()
+squirtle = Pokemon()
+pikachu.name = "피카츄"
+pikachu.nemesis = squirtle
+print(pikachu.name)
+pikachu.nemesis.name = "꼬부기"  # squirtle.name = "꼬부기"
+print(pikachu.nemesis.name)
