cef90b5666f874b0f2246708d32e5e3dca3fa085
bugbuger97 <junopaeng@gmail.com>
2024-01-22 13:15:40 +0900

    day7_method_type2

--- day7.py
+++ day7.py
@@ -2,19 +2,11 @@
 # 클래스 메서드 : 메서드 앞에 @classmethod 데코레이터가 있다. 첫번째 인수는 cls(예약어인 클래스가 아닌다른 것)임.
 # 정적 메서드(static method): @staticmethod 데코레이터가 있다. 첫번째 인수는 위와 같이 자신의 객체나 클래스가 아님.
 
-# class method
-class alphabet:
-    cnt = 0
-    def __init__(self):
-        alphabet.cnt += 1
-    def exclaim(self):
-        print("Alphabet")
-    @classmethod
-    def kid(cls):
-        print("Alphabet has",cls.cnt,"little obj!")
-
-a = alphabet()
-b = alphabet()
-c = alphabet()
-d = alphabet()
-alphabet.kid()
+# static method
+class number:
+    @staticmethod
+    def integer():
+        print('int')
+number.integer()
+# 정적 메서드 : 클래스, 객체에 영향을 미치지 못함.
+# 객체를 생성할 필요가 없음.
