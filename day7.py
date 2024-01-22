708fbc2c9a229c9d0617bc772d7f1746501568d9
bugbuger97 <junopaeng@gmail.com>
2024-01-22 13:55:57 +0900

    day7_Module

--- day7.py
+++ day7.py
@@ -1,12 +1,38 @@
-# 인스턴스 메서드 : 메서드 앞에 데코레이터가 업다. 첫번째 인수는 self임.
-# 클래스 메서드 : 메서드 앞에 @classmethod 데코레이터가 있다. 첫번째 인수는 cls(예약어인 클래스가 아닌다른 것)임.
-# 정적 메서드(static method): @staticmethod 데코레이터가 있다. 첫번째 인수는 위와 같이 자신의 객체나 클래스가 아님.
+# module
+from mymath import isprime, fahrenheit_to_celsius, celsius_to_fahrenheit
+# import mymath as mm
+# from mymath import *
+# import mymath
 
-# static method
-class number:
-    @staticmethod
-    def integer():
-        print('int')
-number.integer()
-# 정적 메서드 : 클래스, 객체에 영향을 미치지 못함.
-# 객체를 생성할 필요가 없음.
+while True:
+    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
+
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {fahrenheit_to_celsius(fahrenheit):.4f}C')
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}C, Fahrenheit : {celsius_to_fahrenheit(celsius):.4f}F')
+    elif menu == '3':
+        number = int(input("Input number : "))
+        if isprime(number):
+            print(f'{number} is prime number')
+        else:
+            print(f'{number} is NOT prime number!')
+    elif menu == '4':
+        numbers = input("Input first second number : ").split()
+        n1 = int(numbers[0])
+        n2 = int(numbers[1])
+
+        if n1 > n2:
+            n1, n2 = n2, n1
+
+        for number in range(n1, n2 + 1):
+            if isprime(number):
+                print(number, end=' ')
+        print()
+    elif menu == '5':
+        print('Terminate Program.')
+        break
+    else:
+        print('Invalid Menu!')
